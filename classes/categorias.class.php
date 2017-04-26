5ff8858ee82e8a516e638a4b727316cd625dd3f5
Marlon Paulo <marlon.paulo.silva@outlook.com>
2017-04-26 11:13:26 -0300

    terminando categorias.class.php

--- classes/categorias.class.php
+++ classes/categorias.class.php
@@ -1,7 +1,18 @@
 <?php 
-class Categoria(){
+class Categorias(){
 	public function getLista(){
-		
+		$array = array();
+
+		global $pdo;
+
+		$sql = $pdo->query("SELECT * from categorias");
+		$sql->execute();
+
+		if($sql->rowCount() > 0){
+			$array = $sql->fetchAll();
+		}
+
+		return $array;
 	}
 }
 
