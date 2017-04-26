e996f7c643a1bb4cdc5b9ee4f997f31dff16fb5f
Marlon Paulo <marlon.paulo.silva@outlook.com>
2017-04-26 07:49:37 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php require "pages/header.php"; ?>
+
+<div class="container-fluid">
+	<div class="jumbotron">
+		<h2>Nós temos para hoje 280 offertas</h2>
+		<p>Mais de 500 empresas cadastradas</p>
+	</div>
+	<div class="row">
+		<div class="col-sm-3"><h4>Pesquisa Avançada</h4></div>
+		<div class="col-sm-9"><h4>Últimos Anúncios</h4></div>
+	</div>
+</div>
+
+<?php require "pages/footer.php"; ?>
